19f41d8d6fae219eb13c0a762ab0ca3a05b3723c
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-11 14:47:26 -0400

    Removed the 'message' parameter and instead added the 'message-dynamic' & 'message-static' paramters. Also added a conditional statement to the send-slack-message shell script that uses them appropriately.

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
 for webhook in ${CHANNEL_WEBHOOKS}; do
-  curl -X POST -H 'Content-type: application/json' \
-    --data "{ \
-              \"blocks\": \
-                [ \
-                  { \
-                    \"type\": \"header\", \
-                    \"text\": { \
-                      \"type\": \"plain_text\", \
-                      \"text\": \"${HEADER}\" \
-                    } \
-                  }, \
-                  { \
-                    \"type\": \"section\", \
-                    \"text\": \
+  if [[ $MESSAGE_DYNAMIC == "" ]]; then
+    curl -X POST -H 'Content-type: application/json' \
+      --data "{ \
+                \"blocks\": \
+                  [ \
                     { \
-                      \"type\": \"mrkdwn\", \
-                      \"text\": \"${MESSAGE}\" \
+                      \"type\": \"header\", \
+                      \"text\": { \
+                        \"type\": \"plain_text\", \
+                        \"text\": \"${HEADER}\" \
+                      } \
+                    }, \
+                    { \
+                      \"type\": \"section\", \
+                      \"text\": \
+                      { \
+                        \"type\": \"mrkdwn\", \
+                        \"text\": \"${MESSAGE_STATIC}\" \
+                      } \
                     } \
-                  } \
-                ] \
-            }" "${!webhook}"
+                  ] \
+              }" "${!webhook}"
+  else
+    curl -X POST -H 'Content-type: application/json' \
+      --data "{ \
+                \"blocks\": \
+                  [ \
+                    { \
+                      \"type\": \"header\", \
+                      \"text\": { \
+                        \"type\": \"plain_text\", \
+                        \"text\": \"${HEADER}\" \
+                      } \
+                    }, \
+                    { \
+                      \"type\": \"section\", \
+                      \"text\": \
+                      { \
+                        \"type\": \"mrkdwn\", \
+                        \"text\": \"${!MESSAGE_DYNAMIC}\" \
+                      } \
+                    } \
+                  ] \
+              }" "${!webhook}"
+  fi
 done
