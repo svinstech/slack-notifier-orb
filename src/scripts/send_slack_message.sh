1ba21133ee12975e160c3675af0c68c3990844d8
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-11 16:54:37 -0400

    Added logic to the send_slack_message.sh shell script that will process interpolated variables. Removd the new message-dynamic and message-static variables and replaced them with the old message variable.

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -1,49 +1,63 @@
 #!/bin/bash
 
+# PROCESS THE SLACK MESSAGE.
+variables=""
+processedMessage=""
+for word in $MESSAGE
+do
+    maxIndex=$((${#word}-1))
+    firstTwoCharacters=${word:0:2}
+    thirdCharacter=${word:2:1}
+
+    if [ $firstTwoCharacters = "\${" ] && [ $thirdCharacter != "!" ]
+    then
+        # Find next instance of a } to indetify the end of the string interpolation.
+        for index in `seq 2 ${maxIndex}`
+        do
+            character=${word:index:1}
+            if [ ${character} = "}" ]
+            then
+                maxIndex=$((${index}-1))
+                break
+            fi
+        done
+
+        variableName=${word:2:$(($maxIndex-1))}
+        variables="${variables} ${variableName}"
+    fi
+done
+
+# Replace all variables in the string with their corresponding values.
+for variable in ${variables}
+do
+    processedMessage="${processedMessage/\$\{${variable}\}/${!variable}}"
+done
+
+# echo ${processedMessage}
+
+
+
+# SEND THE SLACK MESSAGE
 for webhook in ${CHANNEL_WEBHOOKS}; do
-  if [[ $MESSAGE_DYNAMIC == "" ]]; then
-    curl -X POST -H 'Content-type: application/json' \
-      --data "{ \
-                \"blocks\": \
-                  [ \
-                    { \
-                      \"type\": \"header\", \
-                      \"text\": { \
-                        \"type\": \"plain_text\", \
-                        \"text\": \"${HEADER}\" \
-                      } \
-                    }, \
-                    { \
-                      \"type\": \"section\", \
-                      \"text\": \
-                      { \
-                        \"type\": \"mrkdwn\", \
-                        \"text\": \"${MESSAGE_STATIC}\" \
-                      } \
+  curl -X POST -H 'Content-type: application/json' \
+    --data "{ \
+              \"blocks\": \
+                [ \
+                  { \
+                    \"type\": \"header\", \
+                    \"text\": { \
+                      \"type\": \"plain_text\", \
+                      \"text\": \"${HEADER}\" \
                     } \
-                  ] \
-              }" "${!webhook}"
-  else
-    curl -X POST -H 'Content-type: application/json' \
-      --data "{ \
-                \"blocks\": \
-                  [ \
+                  }, \
+                  { \
+                    \"type\": \"section\", \
+                    \"text\": \
                     { \
-                      \"type\": \"header\", \
-                      \"text\": { \
-                        \"type\": \"plain_text\", \
-                        \"text\": \"${HEADER}\" \
-                      } \
-                    }, \
-                    { \
-                      \"type\": \"section\", \
-                      \"text\": \
-                      { \
-                        \"type\": \"mrkdwn\", \
-                        \"text\": \"${!MESSAGE_DYNAMIC}\" \
-                      } \
+                      \"type\": \"mrkdwn\", \
+                      \"text\": \"${processedMessage}\" \
                     } \
-                  ] \
-              }" "${!webhook}"
-  fi
+                  } \
+                ] \
+            }" "${!webhook}"
 done
