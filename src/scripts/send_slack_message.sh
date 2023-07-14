d7a6340640f8c35b01f8243dc2e5c75603e6caad
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-14 16:05:20 -0400

    Removed commented out function. fixed some grammer.

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -8,7 +8,6 @@ apt update; apt install -y curl;
 # Takes 1 argument - The text to process.
 # All interpolated variables in the text will be replaced with their corresponding values.
 # Also, all escaped double-quotation marks will be replaced with single quotation marks.
-
 processText () {
   local processedText=$1
   local variables=""
@@ -17,16 +16,10 @@ processText () {
   then
     for word in ${processedText}
     do
-        # echo "word: $word"
-
         regex="\\$\{(.+)\}"
         if [[ $word =~ $regex ]]
         then
             local variableName=${BASH_REMATCH[1]}
-
-            # echo "variableName: $variableName"
-            # echo "variable value: ${!variableName}"
-
             variables="${variables} ${variableName}"
         fi
     done
@@ -47,59 +40,8 @@ processText () {
   echo "$processedText"
 }
 
-# processText () {
-#   processedText=$1
-#   variables=""
-
-#   if [[ $processedText ]]
-#   then
-#     for word in ${processedText}
-#     do
-#         maxIndex=$((${#word}-1))
-#         firstTwoCharacters=${word:0:2}
-#         thirdCharacter=${word:2:1}
-
-#         if [ "$firstTwoCharacters" = "\${" ] && [ "$thirdCharacter" != "!" ]
-#         then
-#             # Find next instance of a } to indetify the end of the string interpolation.
-#             for index in $(seq 2 ${maxIndex})
-#             do
-#                 character=${word:index:1}
-#                 if [ "${character}" = "}" ]
-#                 then
-#                     maxIndex=$((index-1))
-#                     break
-#                 fi
-#             done
-
-#             variableName=${word:2:$((maxIndex-1))}
-#             variables="${variables} ${variableName}"
-#         fi
-#     done
-
-#     if [[ $variables ]]
-#     then
-#       # Replace all variables in the string with their corresponding values.
-#       for variable in ${variables}
-#       do
-#           processedText="${processedText//\$\{${variable}\}/${!variable}}"
-#       done
-#     fi
-
-#     # Replace all double-quotation marks with single-quotation marks. (to prevent payload errors below)
-#     processedText="${processedText//"\""/"'"}"
-#   fi
-
-#   echo "$processedText"
-# }
-
-
 processedMessage="$(processText "$MESSAGE")"
 processedHeader="$(processText "$HEADER")"
-
-echo "processedMessage: $processedMessage"
-# echo "processedHeader: $processedHeader"
-
 ##################################
 
 
