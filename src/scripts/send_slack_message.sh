ba24b06bdebba5262fc9358a0c7c1414026766d0
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-14 14:07:10 -0400

    Trying out a regex solution in the shell script

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -4,38 +4,22 @@
 apt update; apt install -y curl; 
 
 ################################## PROCESS THE SLACK MESSAGE & HEADER.
-# processedMessage=${MESSAGE}
-# processedHeader=${HEADER}
 
 # Takes 1 argument - The text to process.
 # All interpolated variables in the text will be replaced with their corresponding values.
 # Also, all escaped double-quotation marks will be replaced with single quotation marks.
+
 processText () {
-  processedText=$1
-  variables=""
+  local processedText=$1
+  local variables=""
 
   if [[ $processedText ]]
   then
     for word in ${processedText}
     do
-        maxIndex=$((${#word}-1))
-        firstTwoCharacters=${word:0:2}
-        thirdCharacter=${word:2:1}
-
-        if [ "$firstTwoCharacters" = "\${" ] && [ "$thirdCharacter" != "!" ]
+        if [[ $processedText =~ \$\{(\w+)\} ]]
         then
-            # Find next instance of a } to indetify the end of the string interpolation.
-            for index in $(seq 2 ${maxIndex})
-            do
-                character=${word:index:1}
-                if [ "${character}" = "}" ]
-                then
-                    maxIndex=$((index-1))
-                    break
-                fi
-            done
-
-            variableName=${word:2:$((maxIndex-1))}
+            local variableName=${BASH_REMATCH[1]}
             variables="${variables} ${variableName}"
         fi
     done
@@ -55,6 +39,53 @@ processText () {
 
   echo "$processedText"
 }
+
+# processText () {
+#   processedText=$1
+#   variables=""
+
+#   if [[ $processedText ]]
+#   then
+#     for word in ${processedText}
+#     do
+#         maxIndex=$((${#word}-1))
+#         firstTwoCharacters=${word:0:2}
+#         thirdCharacter=${word:2:1}
+
+#         if [ "$firstTwoCharacters" = "\${" ] && [ "$thirdCharacter" != "!" ]
+#         then
+#             # Find next instance of a } to indetify the end of the string interpolation.
+#             for index in $(seq 2 ${maxIndex})
+#             do
+#                 character=${word:index:1}
+#                 if [ "${character}" = "}" ]
+#                 then
+#                     maxIndex=$((index-1))
+#                     break
+#                 fi
+#             done
+
+#             variableName=${word:2:$((maxIndex-1))}
+#             variables="${variables} ${variableName}"
+#         fi
+#     done
+
+#     if [[ $variables ]]
+#     then
+#       # Replace all variables in the string with their corresponding values.
+#       for variable in ${variables}
+#       do
+#           processedText="${processedText//\$\{${variable}\}/${!variable}}"
+#       done
+#     fi
+
+#     # Replace all double-quotation marks with single-quotation marks. (to prevent payload errors below)
+#     processedText="${processedText//"\""/"'"}"
+#   fi
+
+#   echo "$processedText"
+# }
+
 
 processedMessage="$(processText "$MESSAGE")"
 processedHeader="$(processText "$HEADER")"
