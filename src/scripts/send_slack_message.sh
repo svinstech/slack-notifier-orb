dffde952a2c6bb9cbccd442352c5aa76fe80647d
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-14 13:46:59 -0400

    Created a bash function out of the code that processes text. Then added a variable to hold the processed header as well.

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -3,49 +3,62 @@
 ################################## ENSURE THAT CURL IS INSTALLED
 apt update; apt install -y curl; 
 
-################################## PROCESS THE SLACK MESSAGE.
-variables=""
-processedMessage=${MESSAGE}
+################################## PROCESS THE SLACK MESSAGE & HEADER.
+# processedMessage=${MESSAGE}
+# processedHeader=${HEADER}
 
-if [[ $MESSAGE ]]
-then
-  for word in ${MESSAGE}
-  do
-      maxIndex=$((${#word}-1))
-      firstTwoCharacters=${word:0:2}
-      thirdCharacter=${word:2:1}
+# Takes 1 argument - The text to process.
+# All interpolated variables in the text will be replaced with their corresponding values.
+# Also, all escaped double-quotation marks will be replaced with single quotation marks.
+processText () {
+  processedText=$1
+  variables=""
 
-      if [ "$firstTwoCharacters" = "\${" ] && [ "$thirdCharacter" != "!" ]
-      then
-          # Find next instance of a } to indetify the end of the string interpolation.
-          for index in $(seq 2 ${maxIndex})
-          do
-              character=${word:index:1}
-              if [ "${character}" = "}" ]
-              then
-                  maxIndex=$((index-1))
-                  break
-              fi
-          done
-
-          variableName=${word:2:$((maxIndex-1))}
-          variables="${variables} ${variableName}"
-      fi
-  done
-
-  if [[ $variables ]]
+  if [[ $processedText ]]
   then
-    # Replace all variables in the string with their corresponding values.
-    for variable in ${variables}
+    for word in ${processedText}
     do
-        processedMessage="${processedMessage//\$\{${variable}\}/${!variable}}"
+        maxIndex=$((${#word}-1))
+        firstTwoCharacters=${word:0:2}
+        thirdCharacter=${word:2:1}
+
+        if [ "$firstTwoCharacters" = "\${" ] && [ "$thirdCharacter" != "!" ]
+        then
+            # Find next instance of a } to indetify the end of the string interpolation.
+            for index in $(seq 2 ${maxIndex})
+            do
+                character=${word:index:1}
+                if [ "${character}" = "}" ]
+                then
+                    maxIndex=$((index-1))
+                    break
+                fi
+            done
+
+            variableName=${word:2:$((maxIndex-1))}
+            variables="${variables} ${variableName}"
+        fi
     done
+
+    if [[ $variables ]]
+    then
+      # Replace all variables in the string with their corresponding values.
+      for variable in ${variables}
+      do
+          processedText="${processedText//\$\{${variable}\}/${!variable}}"
+      done
+    fi
+
+    # Replace all double-quotation marks with single-quotation marks. (to prevent payload errors below)
+    processedText="${processedText//"\""/"'"}"
   fi
 
-  # Replace all double-quotation marks with single-quotation marks. (to prevent payload errors below)
-  processedMessage="${processedMessage//"\""/"'"}"
-  echo "${processedMessage}"
-fi
+  echo "$processedText"
+}
+
+processedMessage="$(processText $MESSAGE)"
+processedHeader="$(processText $HEADER)"
+
 ##################################
 
 
@@ -59,7 +72,7 @@ for webhook in ${CHANNEL_WEBHOOKS}; do
                     \"type\": \"header\", \
                     \"text\": { \
                       \"type\": \"plain_text\", \
-                      \"text\": \"${HEADER}\" \
+                      \"text\": \"${processedMessage}\" \
                     } \
                   }, \
                   { \
