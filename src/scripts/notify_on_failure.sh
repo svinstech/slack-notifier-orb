eb3e05673a7ae3845db51eb11e318782f0964c0f
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-21 17:55:41 -0400

    syntax tweak

--- src/scripts/notify_on_failure.sh
+++ src/scripts/notify_on_failure.sh
@@ -1,6 +1,6 @@
 if [ "<<parameters.notify-on-success>>" ]
 then
-    echo 'export WHEN="always"'>>$BASH_ENV
+    "echo 'export WHEN=\"always\"'>>$BASH_ENV"
 else
-    echo 'export WHEN="on_fail"'>>$BASH_ENV
+    "echo 'export WHEN=\"on_fail\"'>>$BASH_ENV"
 fi
