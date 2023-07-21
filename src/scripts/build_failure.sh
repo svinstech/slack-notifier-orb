609e44a083b8819247a3c9096ec09c55e526157b
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-21 17:34:51 -0400

    Deleted the new scripts and instead put the each new shell script line in its own 'run' step

--- src/scripts/build_failure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'export BUILD_STATUS=\"<<parameters.fail-text>>\"'>>"$BASH_ENV"
-echo 'export FAILURE=true'>>"$BASH_ENV"
--- src/scripts/build_success.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'export BUILD_STATUS=\"<<parameters.pass-text>>\"'>>"$BASH_ENV"
-echo 'export FAILURE=false'>>"$BASH_ENV"
