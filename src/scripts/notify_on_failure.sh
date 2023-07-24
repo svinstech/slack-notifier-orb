98d6439154b80cb75e3592d615a80dc19753b958
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-24 11:20:02 -0400

    Removed the notify_on_failure.sh script. Changed the notify-on-success parameter to the 'when' parameter and made it an enum identical to the built-in 'when' parameter for the 'run' job (except for the default value)

--- src/scripts/notify_on_failure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NOTIFY_ON_SUCCESS="<<parameters.notify-on-success>>"
-if [ "$NOTIFY_ON_SUCCESS" ]
-then
-    "echo 'export WHEN=\"always\"'>>$BASH_ENV"
-else
-    "echo 'export WHEN=\"on_fail\"'>>$BASH_ENV"
-fi
