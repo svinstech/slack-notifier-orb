c001044c38638c6d69eb5876f36e676538d288cc
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-28 20:42:22 -0400

    Added url to curl command in get_slack_user_info.sh

--- src/scripts/get_slack_user_info.sh
+++ src/scripts/get_slack_user_info.sh
@@ -11,6 +11,7 @@ fi
 ################################## GET ALL VOUCH SLACK USER INFO (saves them to a new file)
 NOTIFIER_BOT_TOKEN=$1
 DESTINATION_FILE='slackUserInfo.json'
-curl -o "$DESTINATION_FILE" -H 'Content-type: application/json' -H "Authorization: Bearer $NOTIFIER_BOT_TOKEN"
+curl -o "$DESTINATION_FILE" -H 'Content-type: application/json' -H "Authorization: Bearer $NOTIFIER_BOT_TOKEN" "https://slack.com/api/users.list?pretty=1"
+
 ##################################
 
