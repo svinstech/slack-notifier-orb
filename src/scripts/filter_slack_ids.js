7d0bf6f7c074a461aea5af80b9bc2799574792af
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-28 20:34:20 -0400

    Recompiled the typescript jont

--- src/scripts/filter_slack_ids.js
+++ src/scripts/filter_slack_ids.js
@@ -1,8 +1,9 @@
 var _a = require('fs'), readFile = _a.readFile, unlink = _a.unlink, _b = require('child_process'), execSync = _b.execSync, exec = _b.exec;
 var lookupTable = {};
+var NOTIFIER_BOT_TOKEN = process.argv[2]; // First argument must be the NOTIFIER_BOT_TOKEN environment variable saved to this CircleCI project.
 var slackUserInfoFilePath = 'slackUserInfo.json', getSlackUserShellScriptFilePath = 'src/scripts/get_slack_user_info.sh', writeLookupTableShellScriptFilePath = 'src/scripts/writeLookupTableToFile.sh', lookupTableFilePath = 'slackIdLookupTable.json';
 // Execure the shell script that fetches the Slack user info.
-execSync("sh ".concat(getSlackUserShellScriptFilePath));
+execSync("sh ".concat(getSlackUserShellScriptFilePath, " ").concat(NOTIFIER_BOT_TOKEN));
 // Parse the Slack user info to create the lookup table.
 readFile(slackUserInfoFilePath, { encoding: 'utf-8' }, function (err, data) {
     if (!err) {
