96e3e02e1e7fc7eff61841d2b1bd851a8517bcb8
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-07-28 20:39:44 -0400

    Fixed syntax issue in get_slack_user_info.sh

--- src/scripts/get_slack_user_info.sh
+++ src/scripts/get_slack_user_info.sh
@@ -11,6 +11,6 @@ fi
 ################################## GET ALL VOUCH SLACK USER INFO (saves them to a new file)
 NOTIFIER_BOT_TOKEN=$1
 DESTINATION_FILE='slackUserInfo.json'
-curl -o "$DESTINATION_FILE" -H 'Content-type: application/json' -H 'Authorization: Bearer '"$NOTIFIER_BOT_TOKEN"
+curl -o "$DESTINATION_FILE" -H 'Content-type: application/json' -H "Authorization: Bearer $NOTIFIER_BOT_TOKEN"
 ##################################
 
