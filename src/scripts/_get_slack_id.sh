195513a5cb5b632d8395d957fc7945085d955f4e
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-08-16 11:08:10 -0400

    Added several echoes for debugging

--- src/scripts/_get_slack_id.sh
+++ src/scripts/_get_slack_id.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#testing
+echo "testing 1"
+
 temporaryFileForNames="deleteme_names.txt"
 temporaryFileForIds="deleteme_ids.txt"
 
@@ -12,11 +15,17 @@ global_rematch() {
     done
 }
 
+#testing
+echo "testing 2"
+
 # Process inputs
 # shellcheck disable=SC2153
 input_name=$NAME #$1
 # shellcheck disable=SC2153
 output_variable_name=$VARIABLE #$2
+
+#testing
+echo "testing 3"
 
 # The name whose id to get.
 if [[ -z "$input_name" ]]
@@ -25,11 +34,17 @@ then
     exit 1
 fi
 
+#testing
+echo "testing 4"
+
 if [[ -z $output_variable_name ]]
 then
     echo "Second argument must be the name of the variable in which the ID will be stored."
     exit 1
 fi
+
+#testing
+echo "testing 5"
 
 # Regex expressions
 character_matcher="[A-Za-z0-9_ł\-]+"
@@ -39,14 +54,31 @@ id_regex="\"$character_matcher\":\"(@$character_matcher)\""
 # Get lookup table contents
 lookupTableStringified=$(cat slackIdLookupTable.json)
 
+#testing
+echo "testing 6"
+
 # Create arrays of names and ids
 global_rematch "$lookupTableStringified" "$name_regex" "$temporaryFileForNames"
+
+#testing
+echo "testing 7"
+
 # shellcheck disable=SC2162
 read -a names < "$temporaryFileForNames"
 
+#testing
+echo "testing 8"
+
 global_rematch "$lookupTableStringified" "$id_regex" "$temporaryFileForIds"
+
+#testing
+echo "testing 9"
+
 # shellcheck disable=SC2162
 read -a ids < "$temporaryFileForIds"
+
+#testing
+echo "testing 10"
 
 # Find the ID of input_name
 id=""
@@ -61,6 +93,9 @@ do
     fi
 done
 
+#testing
+echo "testing 11"
+
 # Error handling - Name not found
 if [[ -z "$id" ]]
 then
@@ -70,13 +105,22 @@ then
     exit 1
 fi
 
+#testing
+echo "testing 12"
+
 # Dynamically set the output variable.
 declare "$output_variable_name"="$id"
+
+#testing
+echo "testing 13"
 
 # These files are used to store the output of the global_rematch function. 
 # The motivation to store the output in temporary files was to prevent the output from
 #   cluttering the terminal.
 rm "$temporaryFileForNames"
 rm "$temporaryFileForIds"
+
+#testing
+echo "testing 14"
 
 # return 1
