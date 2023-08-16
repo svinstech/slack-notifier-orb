bdf8be93e9522079468dcb23b89bf210abb037f7
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-08-16 12:50:17 -0400

    Commented out line that linter didnt like

--- src/scripts/writeLookupTableToFile.sh
+++ src/scripts/writeLookupTableToFile.sh
@@ -14,5 +14,5 @@ echo "$lookupTable" > "$lookupTableFilePath"
 #testing
 # shellcheck disable=SC2162
 read -a arr < "$lookupTableFilePath"
-echo "LOOKY HERE: $arr"
+# echo "LOOKY HERE: $arr"
 echo "LOOKY HERE: ${arr[@]}"
