6c15e3b389ec2f064a9ebf229d01b25f3c4acd76
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-20 15:34:40 -0400

    git status

--- src/scripts/gather_slack_ids.sh
+++ src/scripts/gather_slack_ids.sh
@@ -3,5 +3,5 @@
 node src/scripts/filter_slack_ids.js "${!SLACK_BOT_TOKEN}"
 
 #testing
-fileContents=$(cat slackIdLookupTable.txt)
-echo "Slack ID lookup table length: ${#fileContents}."
+# fileContents=$(cat slackIdLookupTable.txt)
+# echo "Slack ID lookup table length (in characters): ${#fileContents}."
