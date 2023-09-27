ff97b28fdf67235395be2e6fde74a640acc699ee
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 15:43:33 -0400

    Removed obsolete comment

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# SLACK_BOT_TOKEN=${!SLACK_BOT_TOKEN}
 getSlackUserShellScriptFilePath='src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh'
 slackUserInfoFilePath='slackUserInfo.json'
 slackGroupInfoFilePath='slackGroupInfo.json'
