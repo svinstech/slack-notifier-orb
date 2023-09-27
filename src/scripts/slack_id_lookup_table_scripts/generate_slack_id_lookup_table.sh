7d997a9109fa0b8978c6946d38e5781127e1918b
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 12:14:30 -0400

    Added logic to make use of process.env when accessing environment vars

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx ts-node src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts "${!SLACK_BOT_TOKEN}"
+npx ts-node src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts #"${!SLACK_BOT_TOKEN}"
--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -4,14 +4,14 @@ import { PopulateLookupTable } from './lookup_table_functions'
 const lookupTable:string[] = [];
 
 async function main() {
-    const notifierBotToken:string = process.argv[2] // First argument must be the notifierBotToken environment variable saved to this CircleCI project.
+    const notifierBotToken:string = process.argv[2] ?? process.env.SLACK_BOT_TOKEN // First argument must be the notifierBotToken environment variable saved to this CircleCI project.
     const lookupTableFileName:string = process.argv[3] // Optional 2nd argument. For if you want to use a custom file name.
 
     const slackUserInfoFilePath:string = 'slackUserInfo.json',
         slackGroupInfoFilePath:string = 'slackGroupInfo.json',
         getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
         writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
-        lookupTableFilePath:string = (lookupTableFileName) ? lookupTableFileName : 'slackIdLookupTable.txt'
+        lookupTableFilePath:string = lookupTableFileName ?? 'slackIdLookupTable.txt'
 
     await PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath, notifierBotToken)
 }
