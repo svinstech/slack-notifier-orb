98b8ad1ac84bf4a5906d1d51f813d5e25cd4cbd9
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 15:41:21 -0400

    Assigned to a variable that path to generate_slack_id_lookup_table.ts

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -4,6 +4,7 @@
 getSlackUserShellScriptFilePath='src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh'
 slackUserInfoFilePath='slackUserInfo.json'
 slackGroupInfoFilePath='slackGroupInfo.json'
+slackIdLookupGeneratorFilePath='src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts'
 
 bash "${getSlackUserShellScriptFilePath}" "${!SLACK_BOT_TOKEN}" "${slackUserInfoFilePath}" "${slackGroupInfoFilePath}"
-npx ts-node src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts 
+npx ts-node "${slackIdLookupGeneratorFilePath}"
