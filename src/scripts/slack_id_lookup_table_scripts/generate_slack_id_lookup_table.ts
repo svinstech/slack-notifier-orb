ff7d402253b48e85205f09e8ea00de245f5748d7
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 14:55:20 -0400

    debugging

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -4,6 +4,10 @@ import { PopulateLookupTable } from './lookup_table_functions'
 const lookupTable:string[] = [];
 
 async function main() {
+    //deleteme
+    console.log(`token length: ${process.env.SLACK_BOT_TOKEN?.length}`)
+
+
     const slackUserInfoFilePath:string = 'slackUserInfo.json',
         slackGroupInfoFilePath:string = 'slackGroupInfo.json',
         getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
