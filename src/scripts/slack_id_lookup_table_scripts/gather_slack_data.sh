664b84ccbece914241c2e67c5eebb897a305f494
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 12:41:10 -0400

    Debgging - added temporary env var named DELETE_ME

--- src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh
+++ src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh
@@ -8,9 +8,16 @@ then
 fi
 ##################################
 
+# # # # # DELETE THIS
+echo "DELETE_ME: $DELETE_ME"
+echo "{!DELETE_ME}: ${!DELETE_ME}"
+# # # # # DELETE THIS
+
 ################################## GET ALL VOUCH SLACK USER & USER GROUP INFO (saves them to a new file)
 DESTINATION_FILE_USERS=$1
 DESTINATION_FILE_GROUPS=$2
-curl -o "$DESTINATION_FILE_USERS" -H 'Content-type: application/json' -H "Authorization: Bearer ${!SLACK_BOT_TOKEN}" "https://slack.com/api/users.list?pretty=1"
-curl -o "$DESTINATION_FILE_GROUPS" -H 'Content-type: application/json' -H "Authorization: Bearer ${!SLACK_BOT_TOKEN}" "https://slack.com/api/usergroups.list?pretty=1"
+curl -o "$DESTINATION_FILE_USERS" -H 'Content-type: application/json' -H "Authorization: Bearer $SLACK_BOT_TOKEN" "https://slack.com/api/users.list?pretty=1"
+curl -o "$DESTINATION_FILE_GROUPS" -H 'Content-type: application/json' -H "Authorization: Bearer $SLACK_BOT_TOKEN" "https://slack.com/api/usergroups.list?pretty=1"
 ##################################
+
+
