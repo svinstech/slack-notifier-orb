d5b038d01e13c927d33e01e1e3c294798e50ecd1
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 14:17:53 -0400

    debugging

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-npx ts-node src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+SLACK_BOT_TOKEN=${!SLACK_BOT_TOKEN}
+npx ts-node src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts 
