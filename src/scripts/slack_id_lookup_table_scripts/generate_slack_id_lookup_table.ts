608c60d6ba2bde5c175dd345f4f352ce89e30752
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 12:25:22 -0400

    Removed lingering reference to the slack bot token variable

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -4,16 +4,13 @@ import { PopulateLookupTable } from './lookup_table_functions'
 const lookupTable:string[] = [];
 
 async function main() {
-    const notifierBotToken:string = process.argv[2] ?? process.env.SLACK_BOT_TOKEN // First argument must be the notifierBotToken environment variable saved to this CircleCI project.
-    const lookupTableFileName:string = process.argv[3] // Optional 2nd argument. For if you want to use a custom file name.
-
     const slackUserInfoFilePath:string = 'slackUserInfo.json',
         slackGroupInfoFilePath:string = 'slackGroupInfo.json',
         getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
         writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
-        lookupTableFilePath:string = lookupTableFileName ?? 'slackIdLookupTable.txt'
+        lookupTableFilePath:string = 'slackIdLookupTable.txt'
 
-    await PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath, notifierBotToken)
+    await PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath)
 }
 
 
