35f52c84c13461c6c4818af3d0aefcf2cf0d0252
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-27 14:28:06 -0400

    Added await to exec command in GetSlackData() function

--- src/scripts/slack_id_lookup_table_scripts/lookup_table_functions.ts
+++ src/scripts/slack_id_lookup_table_scripts/lookup_table_functions.ts
@@ -10,7 +10,7 @@ export async function PopulateLookupTable(_lookupTable:string[], _writeLookupTab
 
 async function GetSlackData(_getSlackUserShellScriptFilePath:string, _slackUserInfoFilePath:string, _slackGroupInfoFilePath:string) {
     // Get the Slack user info.
-    exec(`sh ${_getSlackUserShellScriptFilePath} ${_slackUserInfoFilePath} ${_slackGroupInfoFilePath}`, (error:any, stdout:any, stderr:any) => {
+    await exec(`sh ${_getSlackUserShellScriptFilePath} ${_slackUserInfoFilePath} ${_slackGroupInfoFilePath}`, (error:any, stdout:any, stderr:any) => {
         if (stdout) {
             console.log(`stdout: ${stdout}`);
         }
