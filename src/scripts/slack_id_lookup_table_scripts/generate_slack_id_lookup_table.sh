60c11255588dbafa86c56cb2235c5239ce101d4b
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-28 13:46:59 -0400

    Added env vars for the new directory and set them in the gather-slack-ids.yml command so that they are accessible in generate_slack_id_lookup_table.sh

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-getSlackUserShellScriptFilePath='src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh'
-slackUserInfoFilePath='slackUserInfo.json'
-slackGroupInfoFilePath='slackGroupInfo.json'
-slackIdLookupGeneratorFilePath='src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts'
+# getSlackUserShellScriptFilePath='src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh'
+# slackUserInfoFilePath='slackUserInfo.json'
+# slackGroupInfoFilePath='slackGroupInfo.json'
+# slackIdLookupGeneratorFilePath='src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts'
 
-bash "${getSlackUserShellScriptFilePath}" "${!SLACK_BOT_TOKEN}" "${slackUserInfoFilePath}" "${slackGroupInfoFilePath}"
-npx ts-node "${slackIdLookupGeneratorFilePath}"
+bash "${GET_SLACK_USER_SHELL_SCRIPT_FILE_PATH}" "${!SLACK_BOT_TOKEN}" "${SLACK_DATA_DIRECTORY_PATH}/${SLACK_USER_INFO_FILE_NAME}" "${SLACK_DATA_DIRECTORY_PATH}/${SLACK_GROUP_INFO_FILE_NAME}"
+npx ts-node "${SLACK_ID_LOOKUP_GENERATOR_FILE_PATH}"
--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -1,11 +1,10 @@
 import { PopulateLookupTable } from './lookup_table_functions'
 
 const lookupTable:string[] = [],
-    slackDataDirectory = 'slackData',
-    slackUserInfoFilePath:string = `${slackDataDirectory}/slackUserInfo.json`,
-    slackGroupInfoFilePath:string = `${slackDataDirectory}/slackGroupInfo.json`,
+    slackUserInfoFilePath:string = `${process.env.SLACK_DATA_DIRECTORY_PATH}/${process.env.SLACK_USER_INFO_FILE_NAME}`,
+    slackGroupInfoFilePath:string = `${process.env.SLACK_DATA_DIRECTORY_PATH}/${process.env.SLACK_GROUP_INFO_FILE_NAME}`,
     getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
     writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
-    lookupTableFilePath:string = `${slackDataDirectory}/slackIdLookupTable.txt`
+    lookupTableFilePath:string = `${process.env.SLACK_DATA_DIRECTORY_PATH}/slackIdLookupTable.txt`
 
 PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath)
