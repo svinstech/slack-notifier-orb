e40b8343b9cc9d83d400e5ac1009b7e9c69bb8bf
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-28 13:19:25 -0400

    Changed exec to execSync and readFile to readFileSync within lookup_table_functions.ts. Adjusted the surrounding code accordingly. Also added return types for the functions.

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -1,15 +1,10 @@
 import { PopulateLookupTable } from './lookup_table_functions'
 
-const lookupTable:string[] = [];
+const lookupTable:string[] = [],
+    slackUserInfoFilePath:string = 'slackUserInfo.json',
+    slackGroupInfoFilePath:string = 'slackGroupInfo.json',
+    getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
+    writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
+    lookupTableFilePath:string = 'slackIdLookupTable.txt'
 
-async function main() {
-    const slackUserInfoFilePath:string = 'slackUserInfo.json',
-        slackGroupInfoFilePath:string = 'slackGroupInfo.json',
-        getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
-        writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
-        lookupTableFilePath:string = 'slackIdLookupTable.txt'
-
-    await PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath)
-}
-
-main();
+PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath)
--- src/scripts/slack_id_lookup_table_scripts/lookup_table_functions.ts
+++ src/scripts/slack_id_lookup_table_scripts/lookup_table_functions.ts
@@ -1,107 +1,74 @@
-import { readFile } from 'fs'
-import { exec } from 'child_process'
+import { readFileSync } from 'fs'
+import { execSync } from 'child_process'
 import { SlackUser, SlackGroup, SlackUsersResponseObject, SlackGroupsResponseObject } from './interfaces'
 
-export async function PopulateLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _getSlackUserShellScriptFilePath:string, _slackUserInfoFilePath:string, _slackGroupInfoFilePath:string) {
-    await AddUserDataToLookupTable(_lookupTable, _writeLookupTableShellScriptFilePath, _lookupTableFilePath, _slackUserInfoFilePath);
-    await AddUserGroupDataToLookupTable(_lookupTable, _writeLookupTableShellScriptFilePath, _lookupTableFilePath, _slackGroupInfoFilePath);
+export function PopulateLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _getSlackUserShellScriptFilePath:string, _slackUserInfoFilePath:string, _slackGroupInfoFilePath:string):void {
+    AddUserDataToLookupTable(_lookupTable, _writeLookupTableShellScriptFilePath, _lookupTableFilePath, _slackUserInfoFilePath);
+    AddUserGroupDataToLookupTable(_lookupTable, _writeLookupTableShellScriptFilePath, _lookupTableFilePath, _slackGroupInfoFilePath);
 }
 
-async function AddUserDataToLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _slackUserInfoFilePath:string) {
+function AddUserDataToLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _slackUserInfoFilePath:string):void {
     // Parse the Slack user info and add it to the lookup table.
-    readFile(_slackUserInfoFilePath, {encoding: 'utf-8'}, function(err:any, data:any){
-        if (!err) {
-            const slackUsersResponse:SlackUsersResponseObject = JSON.parse(data);
-            if (!slackUsersResponse.ok) {
-                console.log(`!!! Error - Response object not ok (users). Error: ${slackUsersResponse.error}`)
-                return;
-            }
-            
-            let slackUsers:SlackUser[] = slackUsersResponse.members;
+    const slackUserData:string = readFileSync(_slackUserInfoFilePath, {encoding: 'utf-8'})
+    const slackUsersResponse:SlackUsersResponseObject = JSON.parse(slackUserData);
+    if (!slackUsersResponse.ok) {
+        throw(`!!! Error - Response object not ok (users). Error: ${slackUsersResponse.error}`)
+    }
+    
+    let slackUsers:SlackUser[] = slackUsersResponse.members;
 
-            // Keep only non-deleted users that have first names, last names, & emails.
-            slackUsers = slackUsers.filter((slackUser:SlackUser) => {
-                const userNotDeleted:boolean = !slackUser.deleted
-                const userHasFirstAndLastName:boolean = !!slackUser.profile.first_name && !!slackUser.profile.last_name
-                const userHasEmail:boolean = !!slackUser.profile.email
+    // Keep only non-deleted users that have first names, last names, & emails.
+    slackUsers = slackUsers.filter((slackUser:SlackUser) => {
+        const userNotDeleted:boolean = !slackUser.deleted
+        const userHasFirstAndLastName:boolean = !!slackUser.profile.first_name && !!slackUser.profile.last_name
+        const userHasEmail:boolean = !!slackUser.profile.email
 
-                const userIsReal:boolean = userNotDeleted && userHasFirstAndLastName && userHasEmail
+        const userIsReal:boolean = userNotDeleted && userHasFirstAndLastName && userHasEmail
 
-                return userIsReal
-            })
-            
-            // Populate the lookup table with name::id pairs.
-            slackUsers.forEach((slackUser:SlackUser) => {
-                let name:string = `${slackUser.profile.first_name.toLowerCase()}_${slackUser.profile.last_name.toLowerCase()}`.trim().replace(' ','_');
-                name = name.replace(/_?\(.*\)_?/,'') // Remove "_(text)" from names that have them.
-                name = name.replace('__','_') // Convert double underscores to single underscores.
-                name = name.replace("'","") // Remove single quotes
-                name = name.trim(); // Trim again, in case the changes left spaces on either end.
+        return userIsReal
+    })
+    
+    // Populate the lookup table with name::id pairs.
+    slackUsers.forEach((slackUser:SlackUser) => {
+        let name:string = `${slackUser.profile.first_name.toLowerCase()}_${slackUser.profile.last_name.toLowerCase()}`.trim().replace(' ','_');
+        name = name.replace(/_?\(.*\)_?/,'') // Remove "_(text)" from names that have them.
+        name = name.replace('__','_') // Convert double underscores to single underscores.
+        name = name.replace("'","") // Remove single quotes
+        name = name.trim(); // Trim again, in case the changes left spaces on either end.
 
-                const id:string = slackUser.id;
+        const id:string = slackUser.id;
 
-                _lookupTable.push(`${name}=${id}`)
-            })
-
-            // Execute the shell script that stores the lookup table in a file.
-            exec(`sh ${_writeLookupTableShellScriptFilePath} ${_lookupTableFilePath} '${_lookupTable.join("\n")}' true`, (error:any, stdout:any, stderr:any) => {
-                if (stdout) {
-                    console.log(`stdout: ${stdout}`);
-                }
-
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                }
-
-                if (error) {
-                    throw(`!!! Error writing lookup table to file - ${error}`);
-                }
-            });
-        } else {
-            throw(`!!! Error reading ${_slackUserInfoFilePath} file - ${err}`);
-        }
-    });
+        _lookupTable.push(`${name}=${id}`)
+    })
+    
+    // Execute the shell script that stores the lookup table in a file.
+    const overwriteFile:boolean = true
+    execSync(`sh ${_writeLookupTableShellScriptFilePath} ${_lookupTableFilePath} '${_lookupTable.join("\n")}' ${overwriteFile}`)
 }
 
-async function AddUserGroupDataToLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _slackGroupInfoFilePath:string) {
+function AddUserGroupDataToLookupTable(_lookupTable:string[], _writeLookupTableShellScriptFilePath:string, _lookupTableFilePath:string, _slackGroupInfoFilePath:string):void {
     // Parse the Slack group info and add to the lookup table.
-    readFile(_slackGroupInfoFilePath, {encoding: 'utf-8'}, function(err:any, data:any){
-        if (!err) {
-            const slackGroupsResponse:SlackGroupsResponseObject = JSON.parse(data);
-            if (!slackGroupsResponse.ok) {
-                console.log(`!!! Error - Response object not ok (groups). Error: ${slackGroupsResponse.error}`)
-                return;
-            }
-            
-            let slackGroups:SlackGroup[] = slackGroupsResponse.usergroups;
+    const slackUserGroupData:string = readFileSync(_slackGroupInfoFilePath, {encoding: 'utf-8'})
+    
+    const slackGroupsResponse:SlackGroupsResponseObject = JSON.parse(slackUserGroupData);
+    if (!slackGroupsResponse.ok) {
+        throw(`!!! Error - Response object not ok (groups). Error: ${slackGroupsResponse.error}`)
+    }
+    
+    let slackGroups:SlackGroup[] = slackGroupsResponse.usergroups;
 
-            // Populate the lookup table with name::id pairs.
-            slackGroups.forEach((slackGroup:SlackGroup) => {
-                // Only keep non-deleted groups.
-                if (slackGroup.deleted_by == null) {
-                    const handle:string = slackGroup.handle
-                    const id:string = slackGroup.id;
+    // Populate the lookup table with name::id pairs.
+    slackGroups.forEach((slackGroup:SlackGroup) => {
+        // Only keep non-deleted groups.
+        if (slackGroup.deleted_by == null) {
+            const handle:string = slackGroup.handle
+            const id:string = slackGroup.id;
 
-                    _lookupTable.push(`${handle}=${id}`)
-                }
-            })
-
-            // Execute the shell script that stores the lookup table in a file.
-            exec(`sh ${_writeLookupTableShellScriptFilePath} ${_lookupTableFilePath} '${_lookupTable.join("\n")}' false`, (error:any, stdout:any, stderr:any) => {
-                if (stdout) {
-                    console.log(`stdout: ${stdout}`);
-                }
-
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                }
-
-                if (error) {
-                    throw(`!!! Error appending to lookup table file - ${error}`)
-                }
-            });
-        } else {
-            throw(`!!! Error reading ${_slackGroupInfoFilePath} file - ${err}`);
+            _lookupTable.push(`${handle}=${id}`)
         }
-    });
+    })
+
+    // Execute the shell script that stores the lookup table in a file.
+    const overwrite:boolean = false
+    execSync(`sh ${_writeLookupTableShellScriptFilePath} ${_lookupTableFilePath} '${_lookupTable.join("\n")}' ${overwrite}`);
 }
