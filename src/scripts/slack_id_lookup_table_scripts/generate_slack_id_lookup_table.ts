f1b81c033bf96c189344fc52ccc7553a73d94fda
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-28 13:27:12 -0400

    Made the slack data get stored in a directory called 'slackData'. Added it to the .gitignore

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts
@@ -1,10 +1,11 @@
 import { PopulateLookupTable } from './lookup_table_functions'
 
 const lookupTable:string[] = [],
-    slackUserInfoFilePath:string = 'slackUserInfo.json',
-    slackGroupInfoFilePath:string = 'slackGroupInfo.json',
+    slackDataDirectory = 'slackData',
+    slackUserInfoFilePath:string = `${slackDataDirectory}/slackUserInfo.json`,
+    slackGroupInfoFilePath:string = `${slackDataDirectory}/slackGroupInfo.json`,
     getSlackUserShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/gather_slack_data.sh',
     writeLookupTableShellScriptFilePath:string = 'src/scripts/slack_id_lookup_table_scripts/write_lookup_table_to_file.sh',
-    lookupTableFilePath:string = 'slackIdLookupTable.txt'
+    lookupTableFilePath:string = `${slackDataDirectory}/slackIdLookupTable.txt`
 
 PopulateLookupTable(lookupTable, writeLookupTableShellScriptFilePath, lookupTableFilePath, getSlackUserShellScriptFilePath, slackUserInfoFilePath, slackGroupInfoFilePath)
