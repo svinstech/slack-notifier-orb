f20635c147535708544bb7b7d553b26f80094ee4
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-09-28 13:52:12 -0400

    Made generate_slack_id_lookup_table.sh make the slackData directory

--- src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
+++ src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.sh
@@ -5,5 +5,6 @@
 # slackGroupInfoFilePath='slackGroupInfo.json'
 # slackIdLookupGeneratorFilePath='src/scripts/slack_id_lookup_table_scripts/generate_slack_id_lookup_table.ts'
 
+mkdir "${SLACK_DATA_DIRECTORY_PATH}"
 bash "${GET_SLACK_USER_SHELL_SCRIPT_FILE_PATH}" "${!SLACK_BOT_TOKEN}" "${SLACK_DATA_DIRECTORY_PATH}/${SLACK_USER_INFO_FILE_NAME}" "${SLACK_DATA_DIRECTORY_PATH}/${SLACK_GROUP_INFO_FILE_NAME}"
 npx ts-node "${SLACK_ID_LOOKUP_GENERATOR_FILE_PATH}"
