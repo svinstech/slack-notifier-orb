3d2c175875d2a0b7a675a4652a4c1c24ee3e9c4c
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-10-05 12:33:25 -0400

    debugging

--- src/scripts/ping_the_build_triggerer.sh
+++ src/scripts/ping_the_build_triggerer.sh
@@ -13,11 +13,21 @@ then
 fi
 
 #testing
+echo "WORKFLOW_ID length: ${#WORKFLOW_ID}"
+echo "TOKEN length: ${#TOKEN}"
+testWorkflowID=${!WORKFLOW_ID}
+testToken=${!TOKEN}
+echo "expanded WORKFLOW_ID length: ${#testWorkflowID}"
+echo "expanded TOKEN length: ${#testWorkflowID}"
+
+#testing
 echo "About to do curl request"
 
-test=$(curl -s --request GET "https://circleci.com/api/v2/workflow/${!WORKFLOW_ID}/job" \
+DESTINATION_FILE="testFile.json"
+
+curl -o "$DESTINATION_FILE" -s --request GET "https://circleci.com/api/v2/workflow/${!WORKFLOW_ID}/job" \
 --header "circle-token: ${!TOKEN}" \
---header "content-type: application/json" | jq '.items | .[] | select(.type == "approval") | {approved_by}')
+--header "content-type: application/json"# | jq '.items | .[] | select(.type == "approval") | {approved_by}'
 
 #testing
 echo "Finished curl request"
