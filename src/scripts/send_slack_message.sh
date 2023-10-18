7fe4283e04037e9bc44a97c8452bea0d755bfbbf
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-10-18 14:38:56 -0400

    Added logic that will skip empty webhooks instead of throwing an error.

--- src/scripts/send_slack_message.sh
+++ src/scripts/send_slack_message.sh
@@ -110,6 +110,12 @@ processedHeader="$(processText "$HEADER")"
 
 ################################## SEND THE SLACK MESSAGE (NOTE - the 'text' fields must not be empty. That is why there is a trailing space after ${processedMessage}.)
 for webhook in ${CHANNEL_WEBHOOKS}; do
+  if [ -z "${!webhook}"]
+  then
+      echo "Webhook is empty."
+      continue
+  fi
+
   curl -X POST -H 'Content-type: application/json' \
     --data "{ \
               \"blocks\": \
